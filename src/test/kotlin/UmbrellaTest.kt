749fd2a15f627d6c0b3cbaa3b7ca40c8572ec61e
Squall Huang <selfi_star@yahoo.com.tw>
2021-06-10 11:57:08 +0800

    通過:買3支雨傘、價錢100，晴天，總價為270元

--- src/test/kotlin/UmbrellaTest.kt
+++ src/test/kotlin/UmbrellaTest.kt
@@ -1,11 +1,25 @@
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class UmbrellaTest {
     @Test
     fun totalPrice() {
-        val umbrella = Umbrella()
+        val stubWeather = mockk<IWeather>()
+        every { stubWeather.isSunny() } returns false
+        val umbrella = Umbrella(stubWeather)
         val totalPrice = umbrella.totalPrice(3, 100)
         Assertions.assertEquals(300,totalPrice)
+    }
+
+    @Test
+    fun totalPrice_sunny() {
+        val stubWeather = mockk<IWeather>()
+        every { stubWeather.isSunny() } returns true
+        val umbrella = Umbrella(stubWeather)
+        val totalPrice = umbrella.totalPrice(3, 100)
+        Assertions.assertEquals(270,totalPrice)
     }
 }
